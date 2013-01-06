2b3dacbd54dcaed515c823b743a21acd151bceb3
kxhitiz <nirvanagrg@gmail.com>
2013-01-06 13:54:50 +0545

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faves::Application.config.secret_token = 'a127f03ab9ac9ee3483f87deab20fc6d538a6da7cc2f49d4189121a9dea19e46cb54bfdea85feb4b1f0cbc81fa36389bec6886078f724d29a789445d3b939c68'
