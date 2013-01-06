f1a988d5ce2477a7f0d700b689f3dab5d93b2f22
kxhitiz <nirvanagrg@gmail.com>
2013-01-06 16:08:50 +0545

    Removed articles controller, dashboard addition. Use of twitter bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Faves::Application.routes.draw do
+  get "dashboard/index"
+
   resources :articles
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Faves::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
