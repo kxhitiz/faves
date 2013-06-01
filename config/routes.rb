57ba1c00704114e38171a7911e76ebbbc63a63d3
kxhitiz <nirvanagrg@gmail.com>
2013-06-01 16:33:37 +0545

    Route rf

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Faves::Application.routes.draw do
 
-  resources :faves
-  resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup',to:'users#new'
   match '/signin', to:'sessions#new'
   match '/signout',to:'sessions#destroy',via: :delete
 
-  # resources :users do
-  #   resources :faves
-  # end
+  resources :users do
+    resources :faves
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
